e00ad3007b936d9c488d92ee674a8ce2166f4910
Dan <denisamariacatuna@yahoo.com>
2021-01-18 20:21:12 +0200

    adaugare

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Tangerine:bold,bolditalic|Inconsolata:italic|Droid+Sans"
           rel="stylesheet">
+    <link href="{{ asset('css/mystyle.scss') }}" rel="stylesheet">
 
     <style>
         body {
@@ -24,6 +25,38 @@
 
 </head>
 <body class="antialiased">
+<div class="container-services">
+    <div class="row service-line">
+    <div class="col-md-7 d-flex flex-column justify-content-center">
+    <h2 class="service-line heading">Serviciile noastre</h2>
+
+ <div class="row">
+  <div class="column">
+    <img src="/img/Coafor.jpg" alt="Coafor" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="/img/facial_treatment.jpg" alt="Cosmetica" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="/img/Manichiurafrench.jpg" alt="Manichiura" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="/img/pedichiura.jpg" alt="Pedichiura" style="width:100%">
+  </div>
+ </div>
+<div class="col-lg-4 col-md-4 col-sm-4 service-block iconsalon1">
+   <div style="display: inline-block;">
+        
+           <h4 class="pull-left">Cosmetică</h4>
+          </div>
+      </div>
+
+      <br>
+      <img width="45px" src="/img/cosmeticaicon.png">
+    </br>
+    </div>
+</div>
+ </div>
 @if(request()->query('event') === 'success')
     <div class="event-notification">
         Te-ai programat cu succes!

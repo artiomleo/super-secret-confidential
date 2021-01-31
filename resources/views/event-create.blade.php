769282fae732857fee19a528f6ebfb5557bb2c4e
Artiom <artiomleo@gmail.com>
2021-01-31 12:02:46 +0200

    Navbar user name & responsiveness

--- resources/views/event-create.blade.php
+++ resources/views/event-create.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/mystyle.scss') }}" rel="stylesheet">
 
     <title>Salon Vasilica</title>
 

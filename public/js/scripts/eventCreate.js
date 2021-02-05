af0d5db007683b93d1feb17cfaf790347470d003
Artiom <artiomleo@gmail.com>
2021-02-05 10:22:26 +0200

    fix

--- public/js/scripts/eventCreate.js
+++ public/js/scripts/eventCreate.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 });
 
 function doWork() {
-    setTimeout('$("#submitbutton").removeAttr("disabled")', 500);
+    setTimeout('$("#submitbutton").removeAttr("disabled")', 1000);
     var name = document.getElementById("inputName");
     var email = document.getElementById("inputEmail");
     var phone = document.getElementById("inputPhone");
